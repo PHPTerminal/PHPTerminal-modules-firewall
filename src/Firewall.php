492202293e1ce5078237f942a1df7b26dccf3439
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2024-07-21 16:20:55 +1000

    - Added set commands to configure firewall configuration

--- src/Firewall.php
+++ src/Firewall.php
@@ -27,18 +27,23 @@ class Firewall extends Modules
 
         $this->firewallPackage = new PHPFirewallFirewall;
 
+        $this->getFirewallConfig();
+
+        return $this;
+    }
+
+    protected function getFirewallConfig()
+    {
         $firewall = $this->firewallPackage->getFirewallConfig();
 
         if (isset($firewall['response']['responseCode']) && $firewall['response']['responseCode'] == 0) {
             $this->firewallConfig = $firewall['response']['responseData'];
         }
-
-        return $this;
     }
 
     public function getCommands() : array
     {
-        return
+        $commands =
             [
                 [
                     "availableAt"   => "enable",
@@ -57,26 +62,90 @@ class Firewall extends Modules
                     "command"       => "show filter",
                     "description"   => "show filter {address}",
                     "function"      => "show"
-                ],
-                [
-                    "availableAt"   => "config",
-                    "command"       => "filter add",
-                    "description"   => "filter add {filter_type} {address_type} {ip_address|network/subnet|region/{country|state|city}}. filter_type options: allow, block, monitor. address_type options: host, network, region.",
-                    "function"      => "filter"
-                ],
-                [
-                    "availableAt"   => "config",
-                    "command"       => "filter update",
-                    "description"   => "filter update {filter_id} {filter_type}. Update filter type. filter_type options: allow, block, monitor.",
-                    "function"      => "filter"
-                ],
-                [
-                    "availableAt"   => "config",
-                    "command"       => "filter remove",
-                    "description"   => "filter remove {filter_id}. Remove a filter",
-                    "function"      => "filter"
                 ]
             ];
+
+        //Set Commands
+        array_push($commands,
+            [
+                "availableAt"   => "config",
+                "command"       => "",
+                "description"   => "set commands",
+                "function"      => ""
+            ],
+            [
+                "availableAt"   => "config",
+                "command"       => "firewall set status",
+                "description"   => "firewall set status {status}. Status options: enable/disable/monitor.",
+                "function"      => "firewall"
+            ],
+            [
+                "availableAt"   => "config",
+                "command"       => "firewall set filter",
+                "description"   => "firewall set filter {ip_type} {status}. Ip_type options: v4/v6, Status options: enable/disable.",
+                "function"      => "firewall"
+            ],
+            [
+                "availableAt"   => "config",
+                "command"       => "firewall set range",
+                "description"   => "firewall set range {range_type} {status}. Range_type options: private/reserved, Status options: enable/disable.",
+                "function"      => "firewall"
+            ],
+            [
+                "availableAt"   => "config",
+                "command"       => "firewall set default filter",
+                "description"   => "firewall set default filter {state}. Status options: allow/block.",
+                "function"      => "firewall"
+            ],
+            [
+                "availableAt"   => "config",
+                "command"       => "firewall set auto unblock",
+                "description"   => "firewall set auto unblock {minutes}. After X minutes firewall will auto unblock IP from block state. Disable the feature by setting minutes to 0.",
+                "function"      => "firewall"
+            ],
+            [
+                "availableAt"   => "config",
+                "command"       => "firewall set ip2location key",
+                "description"   => "firewall set ip2location key {key}. Set key as null to remove the key",
+                "function"      => "firewall"
+            ]
+        );
+
+        //Filter Commands
+        array_push($commands,
+            [
+                "availableAt"   => "config",
+                "command"       => "",
+                "description"   => "",
+                "function"      => ""
+            ],
+            [
+                "availableAt"   => "config",
+                "command"       => "",
+                "description"   => "filter commands",
+                "function"      => ""
+            ],
+            [
+                "availableAt"   => "config",
+                "command"       => "filter add",
+                "description"   => "filter add {filter_type} {address_type} {ip_address|network/subnet|region/{country|state|city}}. filter_type options: allow, block, monitor. address_type options: host, network, region.",
+                "function"      => "filter"
+            ],
+            [
+                "availableAt"   => "config",
+                "command"       => "filter update",
+                "description"   => "filter update {filter_id} {filter_type}. Update filter type. filter_type options: allow, block, monitor.",
+                "function"      => "filter"
+            ],
+            [
+                "availableAt"   => "config",
+                "command"       => "filter remove",
+                "description"   => "filter remove {filter_id}. Remove a filter",
+                "function"      => "filter"
+            ]
+        );
+
+        return $commands;
     }
 
     protected function showFirewall()
@@ -86,6 +155,8 @@ class Firewall extends Modules
 
             return false;
         }
+
+        $this->getFirewallConfig();
 
         $this->terminal->addResponse('', 0, ['Firewall Details' => $this->firewallConfig]);
 
@@ -167,10 +238,181 @@ class Firewall extends Modules
             return true;
         }
 
-        $response = $this->firewallPackage->response->getAllData();
-        if (isset($response['response']['responseCode']) && isset($response['response']['responseMessage'])) {
-            $this->terminal->addResponse($response['response']['responseMessage'], $response['response']['responseCode']);
+        $this->addFirewallResponseToTerminalResponse();
+
+        return true;
+    }
+
+    protected function firewallSetStatus($args)
+    {
+        if (!$this->firewallConfig) {
+            $this->terminal->addResponse('Error retrieving firewall details. Contact developer!', 1);
+
+            return false;
         }
+
+        if (!isset($args[0])) {
+            $this->terminal->addResponse('Please provide status', 1);
+
+            return false;
+        }
+
+        $firewallConfig = $this->firewallPackage->setConfigStatus($args[0]);
+
+        if ($firewallConfig) {
+            $this->showFirewall();
+
+            return true;
+        }
+
+        $this->addFirewallResponseToTerminalResponse();
+
+        return true;
+    }
+
+    protected function firewallSetFilter($args)
+    {
+        if (!$this->firewallConfig) {
+            $this->terminal->addResponse('Error retrieving firewall details. Contact developer!', 1);
+
+            return false;
+        }
+
+        if (!isset($args[0])) {
+            $this->terminal->addResponse('Please provide filter ip type. Options: v4/v6', 1);
+
+            return false;
+        }
+
+        if (!isset($args[1])) {
+            $this->terminal->addResponse('Please provide filter status. Options enable/disable', 1);
+
+            return false;
+        }
+
+        $firewallConfig = $this->firewallPackage->setConfigFilter($args[0], $args[1]);
+
+        if ($firewallConfig) {
+            $this->showFirewall();
+
+            return true;
+        }
+
+        $this->addFirewallResponseToTerminalResponse();
+
+        return true;
+    }
+
+    protected function firewallSetRange($args)
+    {
+        if (!$this->firewallConfig) {
+            $this->terminal->addResponse('Error retrieving firewall details. Contact developer!', 1);
+
+            return false;
+        }
+
+        if (!isset($args[0])) {
+            $this->terminal->addResponse('Please provide filter range type. Options: private/reserved', 1);
+
+            return false;
+        }
+
+        if (!isset($args[1])) {
+            $this->terminal->addResponse('Please provide filter status. Options enable/disable', 1);
+
+            return false;
+        }
+
+        $firewallConfig = $this->firewallPackage->setConfigRange($args[0], $args[1]);
+
+        if ($firewallConfig) {
+            $this->showFirewall();
+
+            return true;
+        }
+
+        $this->addFirewallResponseToTerminalResponse();
+
+        return true;
+    }
+
+    protected function firewallSetDefaultFilter($args)
+    {
+        if (!$this->firewallConfig) {
+            $this->terminal->addResponse('Error retrieving firewall details. Contact developer!', 1);
+
+            return false;
+        }
+
+        if (!isset($args[0])) {
+            $this->terminal->addResponse('Please provide default filter state. Options: allow/block', 1);
+
+            return false;
+        }
+
+        $firewallConfig = $this->firewallPackage->setConfigDefaultFilter($args[0]);
+
+        if ($firewallConfig) {
+            $this->showFirewall();
+
+            return true;
+        }
+
+        $this->addFirewallResponseToTerminalResponse();
+
+        return true;
+    }
+
+    protected function firewallSetAutoUnblock($args)
+    {
+        if (!$this->firewallConfig) {
+            $this->terminal->addResponse('Error retrieving firewall details. Contact developer!', 1);
+
+            return false;
+        }
+
+        if (!isset($args[0])) {
+            $this->terminal->addResponse('Please provide correct minutes.', 1);
+
+            return false;
+        }
+
+        $firewallConfig = $this->firewallPackage->setConfigAutoUnblockIpMinutes((int) $args[0]);
+
+        if ($firewallConfig) {
+            $this->showFirewall();
+
+            return true;
+        }
+
+        $this->addFirewallResponseToTerminalResponse();
+
+        return true;
+    }
+
+    protected function firewallSetIp2locationKey($args)
+    {
+        if (!$this->firewallConfig) {
+            $this->terminal->addResponse('Error retrieving firewall details. Contact developer!', 1);
+
+            return false;
+        }
+
+        if (!isset($args[0])) {
+            $this->terminal->addResponse('Please provide correct key', 1);
+
+            return false;
+        }
+
+        $firewallConfig = $this->firewallPackage->setConfigIp2locationKey($args[0]);
+
+        if ($firewallConfig) {
+            $this->showFirewall();
+
+            return true;
+        }
+
+        $this->addFirewallResponseToTerminalResponse();
 
         return true;
     }
@@ -219,10 +461,7 @@ class Firewall extends Modules
             return true;
         }
 
-        $response = $this->firewallPackage->response->getAllData();
-        if (isset($response['response']['responseCode']) && isset($response['response']['responseMessage'])) {
-            $this->terminal->addResponse($response['response']['responseMessage'], $response['response']['responseCode']);
-        }
+        $this->addFirewallResponseToTerminalResponse();
 
         return true;
     }
@@ -261,10 +500,7 @@ class Firewall extends Modules
             return true;
         }
 
-        $response = $this->firewallPackage->response->getAllData();
-        if (isset($response['response']['responseCode']) && isset($response['response']['responseMessage'])) {
-            $this->terminal->addResponse($response['response']['responseMessage'], $response['response']['responseCode']);
-        }
+        $this->addFirewallResponseToTerminalResponse();
 
         return true;
     }
@@ -293,13 +529,9 @@ class Firewall extends Modules
             return true;
         }
 
-        $response = $this->firewallPackage->response->getAllData();
-        if (isset($response['response']['responseCode']) && isset($response['response']['responseMessage'])) {
-            $this->terminal->addResponse($response['response']['responseMessage'], $response['response']['responseCode']);
-        }
+        $this->addFirewallResponseToTerminalResponse();
 
         return true;
-
     }
 
     public function onInstall() : object
@@ -308,5 +540,14 @@ class Firewall extends Modules
             'PHPTerminal-modules-firewall is an firewall module for PHPTerminal to manage PHPFirewall library.';
 
         return $this;
+    }
+
+    protected function addFirewallResponseToTerminalResponse()
+    {
+        $response = $this->firewallPackage->response->getAllData();
+
+        if (isset($response['response']['responseCode']) && isset($response['response']['responseMessage'])) {
+            $this->terminal->addResponse($response['response']['responseMessage'], $response['response']['responseCode']);
+        }
     }
 }
